7be174fac6a9c95f18369d3f73a8573a03d92bfb
lmh <207374233@qq.com>
2020-09-04 18:04:55 +0800

    2020-09-04

---
+++ pages/register/index.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+import { View } from 'react-native'
+import { connect } from 'react-redux'
+
+class Register extends React.Component{
+    render() {
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
+
+
+export default connect()(Register)
