6e2c01a61456b7f04063dff168be803566a65922
lmh <207374233@qq.com>
2020-09-17 17:36:34 +0800

    1

---
+++ pages/videoPlay/index.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { View } from 'react-native'
+
+class VideoPlay extends React.Component{
+    render(){
+        return(
+            <View>
+
+            </View>
+        )
+    }
+}
+
+export default VideoPlay
